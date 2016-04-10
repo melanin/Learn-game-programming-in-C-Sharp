3972a50e8e54315f65392a37c9190ef0d7113b20
melanin <melanin951@gmail.com>
2016-04-10 21:25:01 +0900

    02장. C#언어 문법 - 형변환
    float에 int값을 넣는건 가능하지만
    int에 float를 넣는건 에러 처리가 된다

--- BasedLearning/Program.cs
+++ BasedLearning/Program.cs
@@ -10,7 +10,19 @@ namespace BasedLearning
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello C#");
+            float PI = 3.141593F;
+            int PI2 = (int)PI;
+
+            //System.Console.WriteLine("PI = " + PI);
+            //System.Console.WriteLine("PI2 = " + PI2);
+
+            int PI3 = 5;
+
+            PI = PI3;
+            System.Console.WriteLine("PI = {0}", PI);
+
+            //PI3 = PI;//에러 난다
+            //System.Console.WriteLine("PI3 = {0}", PI3);
         }
     }
 }
