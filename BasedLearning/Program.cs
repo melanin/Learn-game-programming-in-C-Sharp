3e4315bf627f12d0803dd62ef6ba3ae4b394cff6
melanin <melanin951@gmail.com>
2016-04-13 23:56:32 +0900

    02장. C#언어 문법: 클래스와 객체

--- BasedLearning/Program.cs
+++ BasedLearning/Program.cs
@@ -7,17 +7,56 @@ using System.IO;
 
 namespace BasedLearning
 {
+    public class Character
+    {
+        public string _name;
+        public int _level;
+        public int _energy;
+
+        public void PrintName()
+        {
+            System.Console.WriteLine("이름: {0}", _name);
+        }
+        public void PrintLevel()
+        {
+            System.Console.WriteLine("레벨: {0}", _level);
+        }
+        public void PrintEnergy()
+        {
+            System.Console.WriteLine("에너지: {0}", _energy);
+        }
+
+        public void PrintAllInformation()
+        {
+            PrintName();
+            PrintLevel();
+            PrintEnergy();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            DirectoryInfo directoryInfo = new DirectoryInfo(Directory.GetCurrentDirectory());
-            FileInfo[] fileInfos = directoryInfo.GetFiles();
+            Character knight = new Character();
+            knight._name = "기사";
+            knight._level = 1;
+            knight._energy = 10;
+            //knight.PrintName();
+            //knight.PrintLevel();
+            //knight.PrintEnergy();
+            knight.PrintAllInformation();
 
-            foreach(FileInfo info in fileInfos)
-            {
-                System.Console.WriteLine(info.Name);
-            }
+            System.Console.WriteLine();
+
+            Character wizard = new Character();
+            wizard._name = "마법사";
+            wizard._level = 2;
+            wizard._energy = 5;
+            //wizard.PrintName();
+            //wizard.PrintLevel();
+            //wizard.PrintEnergy();
+            wizard.PrintAllInformation();
         }
     }
 }
