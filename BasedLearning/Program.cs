ed87e869ac22f1697476b29e224b7ce490372565
melanin <melanin951@gmail.com>
2016-05-02 07:46:22 +0900

    [02장. C#언어 문법] 함수, 메소드 - 매개변수 전달: out

--- BasedLearning/Program.cs
+++ BasedLearning/Program.cs
@@ -15,13 +15,31 @@ namespace BasedLearning
             return __param;
         }
 
+        public static int GetValue2(out int __param)
+        {
+            __param = 20;
+            return __param;
+        }
+
+        //public static int GetValue3(out int __param)
+        //{
+        //    __param++;//out 키워드에 값을 할당하는 것이 아닌 변경하려는 시도는 불가
+        //    return __param;
+        //}
+
         static void Main(string[] args)
         {
-            int param = 2;
-            int ret = GetValue(ref param);
+            int param/* = 2*/;
+            //int ret = GetValue(ref param);//초기화 되지 않은 변수를 ref로 전달 불가
 
-            System.Console.WriteLine(param);//param이 call by value로 전달되어 GetValue를 호출해도 변경되지 않는다
-            System.Console.WriteLine(ret);
+            //System.Console.WriteLine(param);
+            //System.Console.WriteLine(ret);
+
+            int param2;
+            int ret2 = GetValue2(out param2);
+
+            System.Console.WriteLine(param2);
+            System.Console.WriteLine(ret2);
         }
     }
 }
