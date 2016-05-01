34c54780aac3eb9f9ddaa032f34cd5ecf558b3bd
melanin <melanin951@gmail.com>
2016-05-02 07:30:05 +0900

    02장. C#언어 문법: 함수, 메소드 - 매개변수 전달: Call by Value

--- BasedLearning/Program.cs
+++ BasedLearning/Program.cs
@@ -9,22 +9,19 @@ namespace BasedLearning
 {
     class Core
     {
-        public static void PrintText()
+        public static int GetValue(int __param)
         {
-            System.Console.WriteLine("Core::PrintText()");
-        }
-
-        public static int Foo(int __x)
-        {
-            return __x * __x;
+            __param++;
+            return __param;
         }
 
         static void Main(string[] args)
         {
-            PrintText();//static인 Main에서 호출하므로 PrintText도 static이여야 한다
+            int param = 2;
+            int ret = GetValue(param);
 
-            int r = Foo(2);
-            System.Console.WriteLine(r);
+            System.Console.WriteLine(param);//param이 call by value로 전달되어 GetValue를 호출해도 변경되지 않는다
+            System.Console.WriteLine(ret);
         }
     }
 }
