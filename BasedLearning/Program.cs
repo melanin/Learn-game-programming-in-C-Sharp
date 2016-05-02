3301798fbaedf9a576eeca29ff1aa953d9547540
melanin <melanin951@gmail.com>
2016-05-03 08:29:18 +0900

    [02장. C#언어 문법] 함수, 메소드 - class 객체의 reference 참조

--- BasedLearning/Program.cs
+++ BasedLearning/Program.cs
@@ -7,39 +7,30 @@ using System.IO;
 
 namespace BasedLearning
 {
+    class Data
+    {
+        public int a;
+        public int b;
+    }
+
     class Core
     {
-        public static int GetValue(ref int __param)
+        static void Calc(Data __obj)
         {
-            __param++;
-            return __param;
+            __obj.a = 100;
+            __obj.b = 200;
         }
-
-        public static int GetValue2(out int __param)
-        {
-            __param = 20;
-            return __param;
-        }
-
-        //public static int GetValue3(out int __param)
-        //{
-        //    __param++;//out 키워드에 값을 할당하는 것이 아닌 변경하려는 시도는 불가
-        //    return __param;
-        //}
 
         static void Main(string[] args)
         {
-            int param/* = 2*/;
-            //int ret = GetValue(ref param);//초기화 되지 않은 변수를 ref로 전달 불가
+            Data obj = new Data();
+            obj.a = 10;
+            obj.b = 20;
 
-            //System.Console.WriteLine(param);
-            //System.Console.WriteLine(ret);
+            System.Console.WriteLine("초기값 {0}, {1}", obj.a, obj.b);
 
-            int param2;
-            int ret2 = GetValue2(out param2);
-
-            System.Console.WriteLine(param2);
-            System.Console.WriteLine(ret2);
+            Calc(obj);
+            System.Console.WriteLine("결과 {0}, {1}", obj.a, obj.b);//기본 타입을 제외한 타입은 reference로 전달
         }
     }
 }
