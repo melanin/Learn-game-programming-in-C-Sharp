0e099087a573ce39ef76bc19b6f3cd730a703e9f
melanin <melanin951@gmail.com>
2016-05-09 01:44:16 +0900

    [02장. C#언어 문법] 상속과 다형성 - 보호클래스(sealed)

--- BasedLearning/Program.cs
+++ BasedLearning/Program.cs
@@ -6,97 +6,24 @@ using System.Collections;
 
 namespace BasedLearning
 {
-    interface ISort
+    sealed class Animal
     {
-        int GetY();
+        private string name;
+
+        //public virtual void Eat()//Animal클래스는 보호 클래스로 가상 함수 사용 불가
+        //{
+        //    System.Console.WriteLine("{0} eat the rice", name);
+        //}
     }
 
-    class GameObject : ISort
-    {
-        public int x;
-        public int y;
-
-        public int GetY()//인터페이스 ISort의 GetY() 구현
-        {
-            return y;
-        }
-
-        public void Print()
-        {
-            System.Console.WriteLine("===");
-            System.Console.WriteLine("X = {0}", x);
-            System.Console.WriteLine("Y = {0}", y);
-            System.Console.WriteLine("===");
-        }
-    }
+    //class Person : Animal//Animal클래스는 보호 클래스로 상속 불가
+    //{
+    //}
 
     class Core
     {
-        public static void SortGameObject(ArrayList __list)
-        {
-            int min;
-            int minIndex;
-            object temp;
-
-            for (int i = 0; i < __list.Count - 1; i++)
-            {
-                minIndex = i;
-                min = ((ISort)__list[i]).GetY();
-                temp = __list[i];
-
-                for (int k = i + 1; k < __list.Count; k++)
-                {
-                    if (min > ((ISort)__list[k]).GetY())
-                    {
-                        min = ((ISort)__list[k]).GetY();
-                        minIndex = k;
-                    }
-                }
-
-                temp = __list[minIndex];
-                __list[minIndex] = __list[i];
-                __list[i] = temp;
-            }
-        }
-
         static void Main(string[] args)
         {
-            Random rnd = new Random();
-            ArrayList list = new ArrayList();
-
-            GameObject obj1 = new GameObject();
-            obj1.x = rnd.Next(100);
-            obj1.y = rnd.Next(100);
-
-            GameObject obj2 = new GameObject();
-            obj2.x = rnd.Next(100);
-            obj2.y = rnd.Next(100);
-
-            GameObject obj3 = new GameObject();
-            obj3.x = rnd.Next(100);
-            obj3.y = rnd.Next(100);
-
-            GameObject obj4 = new GameObject();
-            obj4.x = rnd.Next(100);
-            obj4.y = rnd.Next(100);
-
-            GameObject obj5 = new GameObject();
-            obj5.x = rnd.Next(100);
-            obj5.y = rnd.Next(100);
-
-            list.Add(obj1);
-            list.Add(obj2);
-            list.Add(obj3);
-            list.Add(obj4);
-            list.Add(obj5);
-
-            SortGameObject(list);
-
-            for (int i = 0; i < list.Count; i++)
-            {
-                GameObject obj = (GameObject)list[i];
-                obj.Print();
-            }
         }
     }
 }
