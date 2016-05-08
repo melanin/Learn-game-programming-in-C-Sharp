b0fcd0475fa66230688d3adee6cb2356cb40ba35
melanin <melanin951@gmail.com>
2016-05-09 07:26:44 +0900

    [02장. C#언어 문법] Object클래스

--- BasedLearning/Program.cs
+++ BasedLearning/Program.cs
@@ -2,28 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Collections;
 
 namespace BasedLearning
 {
-    sealed class Animal
+    class UserType
     {
-        private string name;
-
-        //public virtual void Eat()//Animal클래스는 보호 클래스로 가상 함수 사용 불가
-        //{
-        //    System.Console.WriteLine("{0} eat the rice", name);
-        //}
     }
-
-    //class Person : Animal//Animal클래스는 보호 클래스로 상속 불가
-    //{
-    //}
 
     class Core
     {
         static void Main(string[] args)
         {
+            int a = 10;
+            int b = 10;
+            System.Console.WriteLine("Hashcode is {0}, {1}", a.GetHashCode(), b.GetHashCode());
         }
     }
 }
