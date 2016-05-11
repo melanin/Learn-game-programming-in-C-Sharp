c8c38c0c859a5e29537b37f06ffc8e683d55fc82
melanin <melanin951@gmail.com>
2016-05-12 02:48:34 +0900

    [02장. C#언어 문법] Collection - Stack

--- BasedLearning/Program.cs
+++ BasedLearning/Program.cs
@@ -6,55 +6,47 @@ using System.Collections;
 
 namespace BasedLearning
 {
-    class SimpleIntVector
-    {
-        private ArrayList data = new ArrayList();
-
-        public void push_back(int __element)
-        {
-            data.Add(__element);
-        }
-
-        public int GetData(int __index)
-        {
-            return (int)data[__index];
-        }
-
-        //인덱서
-        public int this[int __index]
-        {
-            get
-            {
-                return (int)data[__index];
-            }
-            set
-            {
-                if (__index >= data.Count)
-                {
-                    throw new IndexOutOfRangeException();
-                }
-                data[__index] = value;
-            }
-        }
-    }
-
     class Core
     {
+        static void PrintAll(IEnumerator<int> __stack)
+        {
+            __stack.Reset();//Stack의 첫번째 요소에 열거자 설정
+            System.Console.Write("Stack Data => ");
+            while (__stack.MoveNext())//Stack의 다음 요소로 이동
+            {
+                System.Console.Write("{0} ", __stack.Current);//Stack의 현재 요소 출력
+            }
+            System.Console.WriteLine();
+        }
+
         static void Main(string[] args)
         {
-            SimpleIntVector vector = new SimpleIntVector();
+            Stack<int> data = new Stack<int>();//Generic Stack.. int형 고정
 
-            vector.push_back(1);
-            vector.push_back(2);
-            vector.push_back(5);
-            vector.push_back(10);
+            data.Push(1);
+            data.Push(2);
+            data.Push(3);
 
-            System.Console.WriteLine("{0} ", vector.GetData(2));
+            PrintAll(data.GetEnumerator());//Stack의 열거자를 인자로 넘김
 
-            vector[2] = 20;
-            System.Console.WriteLine("{0} ", vector[2]);
+            data.Push(9);
+            PrintAll(data.GetEnumerator());
 
-            //vector[10] = 2;//IndexOutOfRangeException.. 할당되지 않은 위치에 값을 넣으면 exception 발생하도록 설정되어 있음
+            data.Push(10);
+            PrintAll(data.GetEnumerator());
+
+            int ret = data.Pop();//마지막으로 들어간 요소를 Stack에서 빼냄
+            System.Console.WriteLine("POP: {0}", ret);
+            PrintAll(data.GetEnumerator());
+
+            ret = data.Pop();
+            System.Console.WriteLine("POP: {0}", ret);
+            PrintAll(data.GetEnumerator());
+
+            //////////////////////////////
+            Stack data2 = new Stack();//Non-Generic Stack.. Object타입을 넣을 수 있다
+            data2.Push(100);
+            data2.Push("ABC");
         }
     }
 }
