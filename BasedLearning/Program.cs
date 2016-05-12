a317b2e45ae9af3fc095e7823b436d53b66a346e
melanin <melanin951@gmail.com>
2016-05-13 07:54:17 +0900

    [02장. C#언어 문법] 반영

--- BasedLearning/Program.cs
+++ BasedLearning/Program.cs
@@ -3,67 +3,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Collections;
+using System.Reflection;//for MethodInfo
 
 namespace BasedLearning
 {
-    struct Vec
-    {
-        private float x;
-        private float y;
-
-        public Vec(float x, float y)
-        {
-            this.x = x;
-            this.y = y;
-        }
-
-        public float X
-        {
-            get
-            {
-                return x;
-            }
-            set
-            {
-                y = value;
-            }
-        }
-
-        public float Y
-        {
-            get
-            {
-                return y;
-            }
-            set
-            {
-                y = value;
-            }
-        }       
-
-        public void PrintAll()
-        {
-            System.Console.WriteLine("X = {0}, Y = {1}", x, y);
-        }
-
-        public static Vec operator -(Vec lhs, Vec rhs)
-        {
-            return new Vec(lhs.X - rhs.X, lhs.Y - rhs.Y);
-        }
-    }
-
     class Core
     {
+        class Player
+        {
+            public void Punch()
+            {
+                System.Console.WriteLine("puhch");
+            }
+            public void Kick()
+            {
+                System.Console.WriteLine("kick");
+            }
+            public void Finger()
+            {
+                System.Console.WriteLine("finger");
+            }
+        }
+
+        class PlayerEx : Player
+        {
+            public void Head()
+            {
+                System.Console.WriteLine("head");
+            }
+        }
+
         static void Main(string[] args)
         {
-            Vec a = new Vec(10, 10);
-            a.PrintAll();
+            Player player = new Player();
+            PlayerEx playerEx = new PlayerEx();
 
-            Vec b = new Vec(100, 100);
-            b.PrintAll();
+            Type type = player.GetType();
+            foreach (MethodInfo info in type.GetMethods())
+            {
+                System.Console.WriteLine(info.Name);
+            }
+            MethodInfo[] infos = type.GetMethods();
+            System.Console.WriteLine("COUNT: {0}", infos.GetLength(0));
+            System.Console.WriteLine();
 
-            Vec c = b - a;
-            c.PrintAll();
+            type = player.GetType();
+            foreach (MethodInfo info in type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly))
+            {
+                System.Console.WriteLine(info.Name);
+            }
+            infos = type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
+            System.Console.WriteLine("COUNT: {0}", infos.GetLength(0));
+            System.Console.WriteLine();
+
+            type = playerEx.GetType();
+            foreach (MethodInfo info in type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly))
+            {
+                System.Console.WriteLine(info.Name);
+            }
+            infos = type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
+            System.Console.WriteLine("COUNT: {0}", infos.GetLength(0));
         }
     }
 }
