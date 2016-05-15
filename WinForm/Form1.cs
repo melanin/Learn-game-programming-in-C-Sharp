7b1728903db62d3ed644cbfe044f9210dcdd7810
melanin <melanin951@gmail.com>
2016-05-16 01:19:14 +0900

    [03장. 윈도우즈 프로그래밍] 마법사로 생성

---
+++ WinForm/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm
+{
+    public partial class Form1 : Form//partial키워드로 클래스를 분리해서 코드 작성 가능
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
