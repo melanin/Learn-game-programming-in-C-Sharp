bb41710f5fddb997b4a75a25edc5737297a3c466
melanin <melanin951@gmail.com>
2016-05-21 02:28:30 +0900

    [03장. 윈도우즈 프로그래밍] 버튼 컨트롤 - 사용자 정의 버튼

---
+++ Controls/Customs/CustomButton.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Controls.Customs
+{
+    class CustomButton : Button
+    {
+        private Bitmap _normal;
+        private Bitmap _pushed;
+
+        public CustomButton()
+        {
+            FlatStyle = FlatStyle.Flat;
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            SetStyle(ControlStyles.UserPaint, true);
+            SetStyle(ControlStyles.Selectable, false);//포커스 삭제
+
+            _normal = new Bitmap(global::Controls.Properties.Resources.button_normal);
+            _pushed = new Bitmap(global::Controls.Properties.Resources.button_down);
+        }
+
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            base.OnPaint(pevent);
+
+            if (Capture && ((MouseButtons & MouseButtons.Left) != 0))
+            {
+                this.Image = _pushed;
+            }
+            else
+            {
+                this.Image = _normal;
+            }
+        }
+    }
+}
--- Controls/Form1.Designer.cs
+++ Controls/Form1.Designer.cs
@@ -10,7 +10,7 @@
         /// <summary>
         /// 사용 중인 모든 리소스를 정리합니다.
         /// </summary>
-        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        /// <param name="disposing">관리되는 리D:\_github\Learn-game-programming-in-C-Sharp\Controls\Customs\CustomButton.cs소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.customButton1 = new Controls.Customs.CustomButton();
             this.SuspendLayout();
             // 
             // button1
@@ -43,11 +44,21 @@
             this.button1.Click += new System.EventHandler(this.button1_Click);
             this.button1.Paint += new System.Windows.Forms.PaintEventHandler(this.ButtonPaint);
             // 
+            // customButton1
+            // 
+            this.customButton1.Location = new System.Drawing.Point(169, 49);
+            this.customButton1.Name = "customButton1";
+            this.customButton1.Size = new System.Drawing.Size(75, 23);
+            this.customButton1.TabIndex = 1;
+            this.customButton1.Text = "customButton1";
+            this.customButton1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.customButton1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "버튼 컨트롤";
@@ -58,6 +69,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private Controls.Customs.CustomButton customButton1;
     }
 }
 
